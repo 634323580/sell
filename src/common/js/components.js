a319d1996c5dc591873f8dbd44327b412cf173cd
ChenZhiPeng <634323580@qq.com>
2016-12-09 12:00:26 +0800

    组件懒加载

--- src/common/js/components.js
+++ src/common/js/components.js
@@ -1,8 +1,11 @@
-import goods from 'components/goods/goods';
+// import goods from 'components/goods/goods';
+const goods = resolve => require(['components/goods/goods'], resolve)
 
-import ratings from 'components/ratings/ratings';
+// import ratings from 'components/ratings/ratings';
+const ratings = resolve => require(['components/ratings/ratings'], resolve)
 
-import seller from 'components/seller/seller';
+// import seller from 'components/seller/seller';
+const seller = resolve => require(['components/seller/seller'], resolve)
 
 export default {
     goods,
